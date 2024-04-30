9ca4b6ef09c9aec16475601c7ee2eacb4f91ab51
ElizaBurda <66063113+ElizaBurda@users.noreply.github.com>
2024-04-30 22:16:49 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class Main {
+
+import java.nio.file.AccessDeniedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws UserNotFoundException, AccessDeniedException {  // В нем нужно запросить логин и пароль пользователя, проверить есть ли данная пара "логин и пароль" в массиве пользователей и последним шагом провалидировать возраст.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите логин");
+            String login = scanner.nextLine();
+            System.out.println("Введите пароль");
+            String password = scanner.nextLine();
+            if ("end".equals(login) && "end".equals(password)) {
+                break;
+            }
+            User user = getUserByLoginAndPassword(login, password);
+            validateUser(user);
+            System.out.println("Доступ предоставлен!");
+            System.out.println("Нажмите Enter для продолжения или Enter и end два раза, чтобы закончить программу");
+            scanner.nextLine();
+        }
+        scanner.close();
+        System.out.println("Программа завершена!");
+
+    }
+
+    public static List<User> getUsers() {                         // этот метод должен возвращать список заранее созданных пользователей:
+        List<User> users = new ArrayList<>();
+        User user1 = new User("Валерия", "Lercha4", "1234L", "Lercha@mail.com", Sex.WOMAN, 24);
+        User user2 = new User("Никита", "Nikki", "N4321", "Nikki@gmail.de", Sex.MAN, 17);
+        User user3 = new User("Коля", "Kolya_tik-tok", "111KKK", "kolya@mail.ru", Sex.MAN, 27);
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+
+        return users;
+    }
+
+    public static User getUserByLoginAndPassword(String login, String password) throws UserNotFoundException {  // в этом методе нужно найти соответствие пары логина и пароля пользователя из массива, возвращаемого методом getUsers. Если пользователь не найден, выбрасываем исключение UserNotFoundException, если найден - возвращаем найденного пользователя:
+        List<User> users = getUsers();
+        for (User user : users) {
+            if (password.equals(user.getPassword()) && login.equals(user.getLogin())) {
+                return user;
+            }
+        }
+        throw new UserNotFoundException("Нет такого пользователя!");
+    }
+
+    public static void validateUser(User user) throws AccessDeniedException {                       // метод для проверки возрастра пользователя. Если возраст менее 18 лет, метод должен выбросить исключение AccessDeniedException
+        if (user.getAge() < 18) {                                                                    // 1) Если возраст меньше 18 лет, то срабатывает вот эта ошибка: 2) AccessDeniedException
+            throw new AccessDeniedException("Доступ запрещен, ограничение по возрасту +18!");    // 2) Если возраст меньше 18 лет от 1) формулы
+        }
+    }
+
+    public static int getAllAgeUsers() {
+        List<User> users = getUsers();
+        int countAge = 0;
+        for (User user : users) {
+            countAge += user.getAge();
+        }
+        return countAge;
+    }
 }
